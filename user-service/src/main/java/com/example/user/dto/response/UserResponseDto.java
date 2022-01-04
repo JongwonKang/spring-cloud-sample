af5a46bf440ba535f3b34bb77c5462b0ee24d710
jongwon <wonwonkk@gmail.com>
2022-01-04 16:10:27 +0900

    [user-service]

--- user-service/src/main/java/com/example/user/dto/response/UserResponseDto.java
+++ user-service/src/main/java/com/example/user/dto/response/UserResponseDto.java
@@ -3,6 +3,7 @@ package com.example.user.dto.response;
 import com.example.common.dto.TokenDto;
 import com.example.user.conts.Authority;
 import com.example.user.repository.Member;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -17,6 +18,15 @@ public class UserResponseDto {
     private String email;
     private Authority authority;
     private TokenDto tokenDto;
+
+    @Builder
+    public UserResponseDto(Long id, String loginId, String email, Authority authority, TokenDto tokenDto) {
+        this.id = id;
+        this.loginId = loginId;
+        this.email = email;
+        this.authority = authority;
+        this.tokenDto = tokenDto;
+    }
 
     public static UserResponseDto of(Member user){
         ModelMapper modelMapper = new ModelMapper();
--- user-service/src/main/java/com/example/user/service/AuthService.java
+++ user-service/src/main/java/com/example/user/service/AuthService.java
@@ -1,24 +1,36 @@
 package com.example.user.service;
 
+import com.example.common.component.TokenProvider;
+import com.example.common.dto.TokenDto;
+import com.example.user.component.CustomAuthenticationProvider;
 import com.example.user.dto.request.SigninRequestDto;
 import com.example.user.dto.response.UserResponseDto;
 import com.example.user.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class AuthService {
+    private final TokenProvider tokenProvider;
+    private final CustomAuthenticationProvider customAuthenticationProvider;
     private final MemberRepository memberRepository;
 
     public UserResponseDto signIn(SigninRequestDto signinRequestDto){
         UsernamePasswordAuthenticationToken authenticationToken =
                 new UsernamePasswordAuthenticationToken(signinRequestDto.getUserId(), signinRequestDto.getPassword());
 
-        return null;
+        Authentication authentication = customAuthenticationProvider.authenticate(authenticationToken);
+
+        TokenDto tokenDto = tokenProvider.generateTokenDto(authentication);
+
+        //TODO refreshToken
+
+        return UserResponseDto.builder().build();
     }
 
 }
